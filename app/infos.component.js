d7edcf5c5a2aa71a0dc1784640582d17dd9f0974
ThibaudVnt <B00606866@essec.edu>
2018-06-20 13:51:42 +0200

    Description added

--- app/infos.component.js
+++ app/infos.component.js
@@ -14,8 +14,10 @@ var InfosComponent = (function () {
     function InfosComponent(router) {
         this.router = router;
         this.title = 'Infos';
-        this.description = 'Des infos sur nous';
-        this.content = 'Description à écrire';
+        this.description = 'Qui sommes nous ?';
+        this.content = 'Depuis plus de 20 ans, Jam réunit la fine fleur des musiciens de l’ESSEC. L’association compte parmi ses membres une trentaine d’étudiants passionnés par la musique. Ils ont tous à cœur d’animer le campus en musique et de représenter l’ESSEC à l’extérieur.';
+        this.content2 = 'Du rock au jazz, en passant par le funk ou la pop, tous les styles se rencontrent lors des concerts ou des sessions d’improvisation qui réunissent toutes les générations. Régulièrement sollicitée pour agrémenter des dîners, galas ou cocktails, l’association se réinvente chaque année autour de grands projets musicaux.';
+        this.content3 = "Si vous souhaitez vous aussi nous entendre intérpréter un catalogue de qualité, n'hésitez pas à nous contacter !";
     }
     InfosComponent.prototype.ngOnInit = function () {
     };
